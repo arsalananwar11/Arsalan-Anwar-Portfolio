b36588c0b86e96553fe096b471c876ac243cc8cc
Arsalan Anwar <49951701+arsalananwar11@users.noreply.github.com>
2025-07-24 11:29:54 -0400

    Improved contact me section (#22)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,18 +9,24 @@ export default function Contact() {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageURL("contact/emailIcon.png")} alt="Email icon" className={styles.icon} />
-          <a href="mailto:arsalan.anwar.7777@gmail.com">arsalan.anwar.7777@gmail.com</a>
+      <ul className={styles.linksRow}>
+        <li className={styles.linkRow}>
+          <a href="mailto:arsalan.anwar.7777@gmail.com" className={styles.linkIconBtn}>
+            <img src={getImageURL("contact/emailIcon.png")} alt="Email icon" className={styles.iconRow} />
+            <span className={styles.linkText}>arsalan.anwar.7777@gmail.com</span>
+          </a>
         </li>
-        <li className={styles.link}>
-          <img src={getImageURL("contact/linkedinIcon.png")} alt="LinkedIn icon" className={styles.icon} />
-          <a href="https://www.linkedin.com/in/arsalan-anwar-ai">linkedin.com/in/arsalan-anwar-ai</a>
+        <li className={styles.linkRow}>
+          <a href="https://www.linkedin.com/in/arsalan-anwar-ai" className={styles.linkIconBtn} target="_blank" rel="noopener noreferrer">
+            <img src={getImageURL("contact/linkedinIcon.png")} alt="LinkedIn icon" className={styles.iconRow} />
+            <span className={styles.linkText}>linkedin.com/in/arsalan-anwar-ai</span>
+          </a>
         </li>
-        <li className={styles.link}>
-          <img src={getImageURL("contact/githubIcon.png")} alt="Github icon" className={styles.icon} />
-          <a href="https://github.com/arsalananwar11">github.com/arsalananwar11</a>
+        <li className={styles.linkRow}>
+          <a href="https://github.com/arsalananwar11" className={styles.linkIconBtn} target="_blank" rel="noopener noreferrer">
+            <img src={getImageURL("contact/githubIcon.png")} alt="Github icon" className={styles.iconRow} />
+            <span className={styles.linkText}>github.com/arsalananwar11</span>
+          </a>
         </li>
       </ul>
     </footer>
